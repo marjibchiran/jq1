8be34038162a42c76792b6f790f7530b6c534627
Marjib Chiran <chiranmarjib88@gmail.com>
2023-09-27 20:36:36 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,21 @@
+
+// JQary
+
+$(document).ready(function(){
+
+   $("#myalert").on('click', function(){
+    alert('Hellow Jquary');
+   })
+//show/hide
+$('#show').on('click', function(){
+    $('#h2').show();
+})
+$('#hide').on('click', function(){
+    $('#h2').hide();
+})
+
+
+})
+
+
+
